7fdee502923bc0aca00cf493f9261f16fa5c59c6
rohith4pr <rohith4pr@gmail.com>
2026-09-26 03:12:27 +0530

    fixed CORS issue

--- insta-giveaway/backend/main.py
+++ insta-giveaway/backend/main.py
@@ -30,7 +30,7 @@ cors_origins = [
     origin.strip()
     for origin in os.getenv(
         "CORS_ORIGINS",
-        "http://localhost:4200,http://127.0.0.1:4200,https://rohith4pr.github.io",
+        "http://localhost:4200,http://127.0.0.1:4200,https://rohith4pr.github.io,https://instaluckydraw.online",
     ).split(",")
     if origin.strip()
 ]
